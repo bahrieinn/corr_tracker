02ad23fa981a792c4c1e24ccba5985ca70b79295
Brian Tong <btong34@gmail.com>
2013-04-01 22:55:29 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CorrTracker::Application.config.secret_token = 'c44af1c6b6422ea6371b1d08bff41752e589da617e3061a015a195e1c8579af8cbf52a0f6bfd0a25e0a642a80298070d46cff40246cd64263cbb69ebd4325597'
